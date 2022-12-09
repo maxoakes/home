ae73035e069389219b1fba5d56a238edb2e994b3
Max Oakes <m.oakes017@gmail.com>
2022-12-09 12:17:09 -0800

    added html to jsx. needs much work

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="static">
+          <div className="background"></div>
+      </div>
+      <div className="content">
+          <div id="home">
+              <div className="weather">
+                  <div>
+                      <h2 id="location-title"> </h2>
+                  </div>
+                  <div className="forecast">
+                      <div id="today-panel" className="day" onclick="openTab(event, 'today', 'weather-data')"></div>
+                      <hr className="seperator"/>
+                      <div id="day0-panel" className="day" onclick="openTab(event, 'day0', 'weather-data')"></div>
+                      <hr className="seperator"/>
+                      <div id="day1-panel" className="day" onclick="openTab(event, 'day1', 'weather-data')"></div>
+                      <hr className="seperator"/>
+                      <div id="day2-panel" className="day" onclick="openTab(event, 'day2', 'weather-data')"></div>
+                      <hr className="seperator"/>
+                      <div id="day3-panel" className="day" onclick="openTab(event, 'day3', 'weather-data')"></div>
+                  </div>
+                  <div className="weather-description">
+                      <div id="today" className="weather-data">
+                      </div>
+                      <div id="day0" className="weather-data">
+                      </div>
+                      <div id="day1" className="weather-data">
+                      </div>
+                      <div id="day2" className="weather-data">
+                      </div>
+                      <div id="day3" className="weather-data">
+                      </div>
+                  </div>
+              </div>
+              <div className="feed1">feed1</div>
+              <div className="feed2">feed2</div>
+              <div className="feed3">feed3</div>
+          </div>
+      </div>
     </div>
   );
 }
